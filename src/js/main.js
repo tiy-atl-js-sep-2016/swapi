f6c46bf441205c1647e8076a5a1c0a463e81175c
Brit Butler <redline6561@gmail.com>
2016-10-17 11:58:37 -0400

    Demonstrate use of APIs with AJAX.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,53 @@
+import $ from 'jquery';
+
 function add (x, y) {
   return x + y;
 };
 
 console.log(add(3, 4));
+
+var container = $(".container");
+console.log(container);
+
+
+// https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop
+
+// function getEtsyResults (searchTerm) {
+//   var data = $.ajax({
+//     url: 'https://api.etsy.com/v2/listings/active',
+//     data: {
+//       keywords: searchTerm,
+//       api_key: "h9oq2yf3twf4ziejn10b717i"
+//     }
+//   });
+//   console.log(data);
+// };
+
+// getEtsyResults("whiskey");
+
+var planetId = 1;
+
+function planetTemplate (planet) {
+  return `
+    <li class="planet">
+      ${planet.name} is a pretty ${planet.climate} planet.
+    </li>`;
+}
+
+function addPlanet (data) {
+  var planetHTML = planetTemplate(data);
+  planets.append(planetHTML);
+}
+
+function nextPlanet (event) {
+  var example = $.ajax({
+    url: `http://swapi.co/api/planets/${planetId}`,
+    success: addPlanet
+  });
+  planetId += 1;
+};
+
+var button = $("#getPlanet");
+button.click(nextPlanet);
+
+var planets = $(".planets");
